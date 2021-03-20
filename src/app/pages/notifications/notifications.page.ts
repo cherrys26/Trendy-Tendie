839c1239c60b844a8532fcbfe3eb781856739db3
Steve Chirayath <stevechirayath@gmail.com>
2021-03-20 16:59:27 -0400

    notifications

--- src/app/pages/notifications/notifications.page.ts
+++ src/app/pages/notifications/notifications.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Platform, PopoverController } from '@ionic/angular';
+import { CrytoinfoService } from 'src/app/services/crypto/crytoinfo.service';
 import { LoaderService } from 'src/app/services/loader/loader.service';
+import { StockinfoService } from 'src/app/services/stocks/stockinfo.service';
 import { AlertsComponent } from '../../components/popover/alerts-filter/alerts.component';
 
 @Component({
@@ -10,15 +12,42 @@ import { AlertsComponent } from '../../components/popover/alerts-filter/alerts.c
 })
 export class NotificationsPage implements OnInit {
 
+  positive: any = ''
+  negative: any = ''
+  gainer: any = ''
+  loser: any = ''
 
   constructor(public platform: Platform,
-    private popoverController: PopoverController, private loader: LoaderService) { }
+    private popoverController: PopoverController, private loader: LoaderService, 
+    private stockService: StockinfoService,
+    private cryptoService: CrytoinfoService,
+    ) { }
 
   alerts = "stocks";
 
 
   ngOnInit() {
     this.loading()
+    this.topGainer();
+    this.topLoser();
+    this.trendingNegative();
+    this.trendingPositive();
+  }
+
+  trendingPositive() {
+    this.stockService.getStock('amzn').subscribe(trendPositive => (this.positive = trendPositive))
+  }
+  trendingNegative() {
+    this.stockService.getStock('gme').subscribe(trendNegative => (this.negative = trendNegative))
+
+  }
+  topGainer() {
+    this.stockService.getStock('mu').subscribe(topGainers => (this.gainer = topGainers))
+
+  }
+  topLoser() {
+    this.stockService.getStock('mlhr').subscribe(topLosers => (this.loser = topLosers))
+
   }
 
   async presentPopover(ev: any) {
--- src/app/services/crypto/crytoinfo.service.ts
+++ src/app/services/crypto/crytoinfo.service.ts
@@ -27,16 +27,5 @@ export class CrytoinfoService {
       )
   }
 
-  searchPrice(ticker: string) {
-    return this.http.get
-      (`${this.urlPrice}?symbol=${ticker}&resolution=D&from=${this.startDate}&to=${this.endDate}&token=${this.apiKey}`)
-      .pipe(
-        map(results => results['c']
-        )
-
-      );
-  }
-
-
 }
 
